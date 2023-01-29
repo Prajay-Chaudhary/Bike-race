b67b9972a95cdba6ebbfd0909534d94426020733
Prajay Chaudhary <prajaychaudhary5678@gmail.com>
2023-01-29 21:15:00 +0100

    Made active pseudo class feature functional on navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,8 +4,8 @@ import { Link, useLocation } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
-  const location = useLocation()
-  console.log(location.pathname)
+  const location = useLocation();
+  const pathname = location.pathname;
   const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
   const [color, setColor] = useState(false);
@@ -31,19 +31,21 @@ const Navbar = () => {
           onClick={handleClick}
           className={click ? "nav-menu active" : "nav-menu"}
         >
-          <li>
+          <li className={`${pathname === "/" ? "nav-menu-active" : ""}`}>
             <Link to={"/"}>Home</Link>
           </li>
-          <li>
+          <li className={`${pathname === "/Riders" ? "nav-menu-active" : ""}`}>
             <Link to={"/Riders"}>Riders</Link>
           </li>
-          <li>
+          <li
+            className={`${pathname === "/Location" ? "nav-menu-active" : ""}`}
+          >
             <Link to={"/Location"}>Location</Link>
           </li>
-          <li>
+          <li className={`${pathname === "/Photos" ? "nav-menu-active" : ""}`}>
             <Link to={"/Photos"}>Photos</Link>
           </li>
-          <li>
+          <li className={`${pathname === "/Contest" ? "nav-menu-active" : ""}`}>
             <Link to={"/Contest"}>Contest</Link>
           </li>
         </ul>
